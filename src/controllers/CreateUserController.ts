b706b9e36a3ccb6f2ab23c912f5d17caa839fc0a
Jose Victor Persset <victor.persset@gmail.com>
2021-09-06 10:16:54 -0300

    Fim da aula 3, criação de tags e tratamento de middlewares para rotas

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -2,16 +2,19 @@ import { Request, Response } from "express";
 import { CreateUserService } from "../services/CreateUserService";
 
 class CreateUserController {
+  async handle(request: Request, response: Response) {
+    const { name, email, admin } = request.body;
 
-        async handle(request: Request, response: Response) {
-            const { name, email, admin } = request.body;
+    const createUserService = new CreateUserService();
 
-            const createUserService = new CreateUserService();
+    const user = await createUserService.execute({ name, email, admin });
 
-            const user = await createUserService.execute({ name, email, admin});
-
-            return response.json(user);
-        }
+    return response.json(user);
+  }
 }
 
-export { CreateUserController }
+export { CreateUserController };
+
+/**
+ * server -> routes -> Controller -> Service (throw new Error)
+ */
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,20 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateTagController } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.post("/users", createUserController.handle);
+
+/**
+ * Utilizando o router.use todas as rotas após a declaração do mesmo estarão utilizando o middleware declarado
+ * Declarando o middleware na declaração da rota, entre o path e entre a função da rota, os middlewares serão usados apenas naquela rota
+ * */
+//router.use(ensureAdmin)
+router.post("/tags", ensureAdmin, createTagController.handle);
 
 export { router };
